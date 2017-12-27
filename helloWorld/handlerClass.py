431ac0a6235c641f892c01523899c5360837511d
Shubhasish <panda.subhasish007@gmail.com>
2017-12-27 23:17:20 +0530

    Review Board test

--- helloWorld/handlerClass.py
+++ helloWorld/handlerClass.py
@@ -5,7 +5,7 @@ from flask_restful import Resource
 class HelloWorld(Resource):
 
     def get(self):
-        return flask.jsonify({"Success": "true", "Message": "Hello World, I am doing my first test"})
+        return flask.jsonify({"Success": "true", "Message": "Hello World, I am doing my first test. I am checkinh the diffs."})
 
 
 
