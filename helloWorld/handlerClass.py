6df9d9dfb665b38e4edbfa6a6704df29985996d9
Shubhasish <spanda@alpha-sense.com>
2018-01-06 16:44:48 +0530

    Pre-push hook test

--- helloWorld/handlerClass.py
+++ helloWorld/handlerClass.py
@@ -5,7 +5,7 @@ from flask_restful import Resource
 class HelloWorld(Resource):
 
     def get(self):
-        return flask.jsonify({"Success": "true", "Message": "Hello World, we are testing again."})
+        return flask.jsonify({"Success": "true", "Message": "Hello World, git push test."})
 
 
 
